e4d4ed584a3fcd21419e349f3a33675593d8c0b1
ndmg-dev <nucleodigitalmendoncagalvao@gmail.com>
2026-05-17 17:11:01 -0300

    refactor currency conversion logic and update HTML structure for currency selection

--- scripts.js
+++ scripts.js
@@ -1,16 +1,39 @@
 const convertButton = document.querySelector(".convert-button");
-
+const selectCurrency = document.querySelector(".currency-select");
 
 function convertCurrency() {
     const inputCurrencyValue = document.querySelector(".currency-input").value;
+    const currencyValueToConvert = document.querySelector("#real-value");
+    const convertedCurrencyValue = document.querySelector("#dolar-value");
 
+    console.log(selectCurrency.value);
     const dolarToday= 5.2;  
     const euroToday= 6.2;
 
     const convertedToDolar = inputCurrencyValue / dolarToday;
-    const convertedToEuro = inputCurrencyValue / euroToday;
-    console.log(convertedToDolar);
-    console.log(convertedToEuro);
+
+    if(selectCurrency.value === "dolar") {
+        convertedCurrencyValue.innerHTML = new  Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(convertedToDolar);
+
+    }
+    
+    if(selectCurrency.value === "euro") {
+        convertedCurrencyValue.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday);
+    }
+
+
+    currencyValueToConvert.innerHTML = new  Intl.NumberFormat("pt-br", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue);
+
+
 }
 
 
